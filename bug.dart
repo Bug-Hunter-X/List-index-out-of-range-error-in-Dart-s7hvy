5c2d245e8b5bc3a36b8b870a9a0b26e9521b5d0d
BugHunter-X <askzenai@gmail.com>
2025-02-06 16:18:09 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,9 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+
+// This will throw an error if the list is empty.
+int firstNumber = numbers.first;
+
+// This will throw an error if the list is empty or has fewer than 2 elements.
+int secondNumber = numbers[1];
+```
